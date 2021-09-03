bff159145cbb7cfb5c6d27a738198ecbd1b66fd0
Gusjafo <gustavojafo@gmail.com>
2021-09-03 11:10:44 -0300

    first-commit

--- app.js
+++ app.js
@@ -17,7 +17,7 @@ const auth = require("./middleware/auth");
 app.use("/static", express.static('./static/'));
 
 app.get("/", function(req, res){
-    res.sendFile(__dirname + "/index.html");
+    res.sendFile(__dirname + "./static/index.html");
 });
 
 // Register
